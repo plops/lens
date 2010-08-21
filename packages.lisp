7864d7a21131550881201c2df8af8509fbad5dac
mk <martin@chuck.(none)>
2010-08-21 22:30:13 +0100

    new exports

--- packages.lisp
+++ packages.lisp
@@ -2,4 +2,17 @@
 
 (defpackage :vector
   (:use :cl)
-  (:export #:with-arrays))
+  (:export #:with-arrays
+	   #:vec
+	   #:make-vec
+	   #:v
+	   #:v.
+	   #:v-
+	   #:v+
+	   #:v*
+	   #:vx
+	   #:norm
+	   #:normalize
+	   #:mat
+	   #:rotation-matrix
+	   #:m*))
